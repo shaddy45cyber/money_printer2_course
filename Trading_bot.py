0cb61f341b433a02378aa4506ae6f839a2014456
shaddy45cyber <abrahamagboradam@gmail.com>
2025-09-24 15:50:54 +0000

    say something about ur changes

--- Trading_bot.py
+++ Trading_bot.py
@@ -1,1 +1,29 @@
+import ccxt
+import pandas as pd
+import numpy as np
+from datetime import datetime  as dt, timedelta
+import pytz
+import time
+import warnings
+import tulipy as ti
+
+# public instance 
+exchange = ccxt.binance()
+
+#private instance
+Apikey = "your apikey"
+Secretkey = "your secretkey"
+Exchange = ccxt.binance({
+    'apikey':Apikey,
+    'secret':Secretkey,
+    'enableRateLimit':True,
+    'options':{
+    'defaultType':'future'
+    }
+})
+
+exchange.load_markets()
+symbol = 'DOGEUSDT'
+
+
 
