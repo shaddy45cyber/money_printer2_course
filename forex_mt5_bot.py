fe2447e715976bdbf6ccb6ac183486380765d439
shaddy45cyber <abrahamagboradam@gmail.com>
2025-09-24 16:09:51 +0000

    to be continued

--- forex_mt5_bot.py
+++ forex_mt5_bot.py
@@ -1,1 +1,15 @@
+import MetaTrader5 as mt5
+
+import pandas as pd
+import numpy as np
+import time
+from datetime import datetime as dt , timedelta
+import pytz
+
+symbol = "XAUUSD"
+magic_no = 123456
+
+mt5.initialize()
+#your trading bots 
+mt5.shutdown
 
